45af227122a62081ed666f1cf27cdcce629494f8
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-03-11 15:51:14 -0300

    Implement derivative control

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,33 @@ using namespace std;
 ros::Publisher publisherControlSignal;
 
 float P_gain;
+float I_gain;
+float D_gain;
+
+double lastError;
+ros::Time lastErrorTime;
+bool hasLastError;
 
 void controllerCallback(const std_msgs::Float64::ConstPtr& error)
 {
-    // Proportional control.
-    float controlSignal = error->data * P_gain;
+    double derivative = 0;
+    if (hasLastError) {
+        double de = error->data - lastError;
+        double dt = (ros::Time::now() - lastErrorTime).toSec();
+        derivative = D_gain * (de/dt);
+    }
+
+    double proportional = error->data * P_gain;
+    double controlSignal = proportional + derivative;
 
     geometry_msgs::Twist twistSignal;
     twistSignal.angular.z = controlSignal;
 
     publisherControlSignal.publish(twistSignal);
+
+    lastError = error->data;
+    lastErrorTime = ros::Time::now();
+    hasLastError = true;
 }
 
 int main(int argc, char** argv)
@@ -29,6 +46,14 @@ int main(int argc, char** argv)
         ROS_FATAL("Parameter P_gain not set.");
         return -1;
     }
+
+    if (ros::param::get("~D_gain", D_gain) == false)
+    {
+        ROS_FATAL("Parameter D_gain not set.");
+        return -1;
+    }
+
+    hasLastError = false;
 
     ros::Subscriber subscriberSetPoint = nodeHandle.subscribe("error", 1000, controllerCallback);
 
